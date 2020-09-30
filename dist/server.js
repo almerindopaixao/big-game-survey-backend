2ff54a60ea138705df3bfdf767a01c816554aef4
Almerindo Paixão <almerindopaixao@gmail.com>
2020-09-30 11:53:09 -0300

    Update GET/records

--- dist/server.js
+++ dist/server.js
@@ -6,9 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const port = process.env.APP_PORT || 8080;
 app_1.default.on('ok', () => {
-    app_1.default.listen(port, () => {
-        console.log(`Acessar http://localhost:${port}`);
-        console.log(`Servidor executando na porta ${port}`);
-    });
+    app_1.default.listen(port);
 });
 //# sourceMappingURL=server.js.map
